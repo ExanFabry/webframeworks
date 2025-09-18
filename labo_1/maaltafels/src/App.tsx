f752dc7cca3960727697b91d4da28a47dc246a9b
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2025-09-18 10:37:46 +0200

    Maaltafels

---
+++ labo_1/maaltafels/src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let maaltafels : number[] = [];
+  for(let i : number = 1; i < 11; i++){
+    for(let j : number = 1; j < 11; j++){
+      maaltafels.push(i*j);
+    }
+  }
+
+  return (
+    <>
+      <table>
+        {maaltafels.map((number: number) => <tr>{number}</tr>)}
+      </table>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
