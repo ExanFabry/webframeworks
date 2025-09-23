b90271a9750ad67495f3938e4506f041bc967912
Exan Fabry <exanfabry@gmail.com>
2025-09-23 15:15:16 +0200

    Maaltafels component

---
+++ labo_2/maaltafels-component/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { MultiplicationTable } from './MultiplicationTable';
+
+function App() {
+  return (
+    <>
+      <MultiplicationTable max={5}/>
+    </>
+  )
+}
+
+export default App
